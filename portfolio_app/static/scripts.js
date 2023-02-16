6fc20f99522810725af485c49cd230b9dd56337e
Yenni Vanessa Delgado <yennivanessadelgado@gmail.com>
2023-02-16 13:42:07 -0500

    my first commit

---
+++ portfolio_app/static/scripts.js
@@ -0,0 +1,10 @@
+const toggle = document.querySelector('.toggle')
+const links = document.querySelector('.links')
+
+toggle.addEventListener('click', () =>{
+    toggle.classList.toggle('rotate')
+    links.classList.toggle('active')
+
+}
+
+)
